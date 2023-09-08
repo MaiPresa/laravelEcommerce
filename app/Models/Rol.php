fcad18e5dd768d8c90307c3d5d7ba05b39b3f833
Anais-RV <anaisrv@gmail.com>
2023-09-08 20:09:47 +0200

    fixed conventions models class

--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Rol extends Model
 {
     use HasFactory;
+    public $timestamps = false; 
+    protected $primaryKey = 'id_roles';
 
 public function usuarios(){
     return $this->hasMany(Usuario::class, 'id_roles');
