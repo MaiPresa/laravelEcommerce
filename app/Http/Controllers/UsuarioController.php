ddbc43ca1b4188480bcd7a7b1514f32e8b14ca05
Anais-RV <anaisrv@gmail.com>
2023-09-09 14:12:53 +0200

    use fixed

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Usuario;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class UsuarioController extends Controller
 {
