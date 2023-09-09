759cd2fadab2ad21fff83e84bd48962328f999d3
Anais-RV <anaisrv@gmail.com>
2023-09-09 14:10:35 +0200

    use fixed

--- app/Http/Controllers/RolController.php
+++ app/Http/Controllers/RolController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Rol;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class RolController extends Controller
 {
